f48da6f54fa57df5a4dc7690cb101e1430e77646
phoenixkonsole <40170965+phoenixkonsole@users.noreply.github.com>
2026-06-07 20:28:45 +0200

    Delete src directory

--- src/protocol/qrx_chainid.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#define QRX_CHAIN_ID_MAINNET "qrx-mainnet"
-#define QRX_CHAIN_ID_TESTNET "qrx-testnet"
-#define QRX_CHAIN_ID_DEVNET  "qrx-devnet"
--- src/qrx_main.c
+++
@@ -1,6 +0,0 @@
-#include <openssl/evp.h>
-#include "qrx_core.h"
-int main(int argc, char **argv) {
-    OpenSSL_add_all_algorithms();
-    return qrx_backend_main(argc, argv);
-}
